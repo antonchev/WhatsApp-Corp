32dcfd97b60128800f896757b45637cc3a3c09d1
antonchev <antonchev@mail.ru>
2019-11-04 12:18:45 +0300

    work auth google service

--- app/src/main/java/com/ilocator/UsersPresenter.java
+++ app/src/main/java/com/ilocator/UsersPresenter.java
@@ -10,23 +10,12 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class UsersPresenter {
   //  private static final String TAG = Debug;
-    private static int RC_SIGN_IN = 100;
-    private FirebaseAuth mAuth;
-    private GoogleSignInClient mGoogleSignInClient;
+
     UsersActivity view;
 
 
 
-    public void auth() {
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-              
-                .requestEmail()
-                .build();
 
-        mGoogleSignInClient = GoogleSignIn.getClient(view, gso);
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        view.startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
 
 
     public void attachView(UsersActivity usersActivity) {
